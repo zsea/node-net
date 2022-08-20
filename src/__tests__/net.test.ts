a0971eb9bd0989e5ebe74a12b68b1967eed556b0
曾大海 <zsea@qq.com>
2022-08-21 02:12:18 +0800

    检查tslint

--- src/__tests__/net.test.ts
+++ src/__tests__/net.test.ts
@@ -22,13 +22,8 @@ import { Server, Socket, connect, createConnection, createServer } from '../net'
 // server.listen(9988, function(){
 //   console.log("监听已启动",arguments);
 // })
-(async function () {
+test("Is Socket", () => {
   let socket = createConnection(9988, "127.0.0.1");
-  while (true) {
-    let u = await socket.readString();
-    console.log("读取到", u);
-    socket.writeUInt8(0x1F);
-    
-  }
-  console.log("执行完成")
-})();
+  expect(socket instanceof Socket).toBe(true);
+  socket.destroy();
+});
--- src/net.ts
+++ src/net.ts
@@ -1,23 +1,22 @@
 import { Server as netServer, Socket as netSocket, connect as netConnect, NetConnectOpts, createServer as netCreateServer, ServerOpts, SocketConstructorOpts, SocketConnectOpts } from "net";
 import SocketError from "./error";
-function getBuffer(__buffer__: Buffer | null | undefined, length: number) {
-    if (__buffer__ && __buffer__.length >= length) {
-        let nBuffer = __buffer__.subarray(0, length);
-        //__buffer__ = __buffer__.subarray(length);
+function getBuffer(buffer: Buffer | null | undefined, length: number) {
+    if (buffer && buffer.length >= length) {
+        const nBuffer = buffer.subarray(0, length);
         return nBuffer;
     }
 }
-function getString(__buffer__: Buffer | null | undefined, length: number = Number.MAX_VALUE, end_falg: 0 | 10 = 0) {
-    if (__buffer__ && __buffer__.length) {
+function getString(buffer: Buffer | null | undefined, length: number = Number.MAX_VALUE, endFalg: 0 | 10 = 0) {
+    if (buffer && buffer.length) {
         let endLength = 0;
-        for (let i = 0; i < __buffer__.length; i++) {
-            if (__buffer__[i] === end_falg || i === length - 1) {
+        for (let i = 0; i < buffer.length; i++) {
+            if (buffer[i] === endFalg || i === length - 1) {
                 endLength = i + 1;
                 break;
             }
         }
         if (endLength) {
-            return __buffer__.subarray(0, endLength);
+            return buffer.subarray(0, endLength);
         }
     }
 }
@@ -25,14 +24,14 @@ type BUFFER_CALLBACK = (err: Error | null, buffer?: Buffer) => void;
 class BufferReader {
     length: number
     isString: boolean = false
-    string_end_falg: 0 | 10 = 0
+    stringEndFlag: 0 | 10 = 0
     callback: (err: Error | null, buffer?: Buffer) => void
     constructor(length: number, callback: BUFFER_CALLBACK) {
         this.length = length;
         this.callback = callback;
     }
 }
-
+// tslint:disable-next-line
 class Server extends netServer {
     constructor(options?: ServerOpts, connectionListener?: ((socket: Socket) => void)) {
         super(options);
@@ -44,84 +43,91 @@ class Server extends netServer {
         }
     }
 }
+// tslint:disable-next-line
 class Socket extends netSocket {
-    private __buffer__: Buffer | null | undefined
-    private __readerList__: BufferReader[] = [];
+    private buffer: Buffer | null | undefined
+    private readerList: BufferReader[] = [];
     constructor(options?: any | undefined) {
         let ops: any;
-        if (options && options._handle) {//传入的是一个socket
+        if (options && options._handle) {
+            // tslint:disable-next-line
+            /**传入的是一个socket */
             ops = { handle: options._handle, ...options } as SocketConstructorOpts;
         }
-        else { //其它参数
+        else {
             ops = options;
         }
         super(ops);
         this.addListener("data", (data: Buffer): void => {
-            if (this.__buffer__) {
+            if (this.buffer) {
 
-                this.__buffer__ = Buffer.concat([this.__buffer__, data]);
+                this.buffer = Buffer.concat([this.buffer, data]);
             }
             else {
-                this.__buffer__ = data;
+                this.buffer = data;
             }
             this.onData();
         });
         this.addListener("close", (hadError) => {
-            if (this.__readerList__ && this.__readerList__.length) {
-                let err = new SocketError("socket is closed");
+            if (this.readerList && this.readerList.length) {
+                const err = new SocketError("socket is closed");
                 err.code = "SOCKET_CLOSED";
-                for (let i = 0; i < this.__readerList__.length; i++) {
-                    let item = this.__readerList__[i];
+                // tslint:disable-next-line
+                for (let i = 0; i < this.readerList.length; i++) {
+                    const item = this.readerList[i];
                     item.callback(err);
                 }
             }
-            this.__readerList__ = [];
+            this.readerList = [];
         })
     }
     private onData(): void {
-        while (this.__readerList__ && this.__readerList__.length) {
-            let item = this.__readerList__[0];
+        while (this.readerList && this.readerList.length) {
+            const item = this.readerList[0];
             let nBuffer: Buffer | undefined;
             if (item.isString) {
-                nBuffer = getString(this.__buffer__, item.length, item.string_end_falg);
+                nBuffer = getString(this.buffer, item.length, item.stringEndFlag);
             }
             else {
-                nBuffer = getBuffer(this.__buffer__, item.length);
+                nBuffer = getBuffer(this.buffer, item.length);
             }
-            //console.log("读取到",nBuffer)
             if (nBuffer) {
 
-                this.__buffer__ = this.__buffer__?.subarray(item.length);
-                this.__readerList__.shift();
+                this.buffer = this.buffer?.subarray(item.length);
+                this.readerList.shift();
                 item.callback(null, nBuffer);
                 continue;
             }
             break;
         }
     }
-    private readBuffer(length: number, callback: BUFFER_CALLBACK, isString: boolean = false, end_falg: 0 | 10 = 0): void {
+    private readBuffer(length: number, callback: BUFFER_CALLBACK, isString: boolean = false, endFalg: 0 | 10 = 0): void {
         let nBuffer: Buffer | undefined;
         if (isString) {
-            nBuffer = getString(this.__buffer__, length, end_falg);
+            nBuffer = getString(this.buffer, length, endFalg);
         }
         else {
-            nBuffer = getBuffer(this.__buffer__, length);
+            nBuffer = getBuffer(this.buffer, length);
         }
         if (nBuffer) {
             callback(null, nBuffer);
         }
         else {
-            this.__readerList__.push({
+            this.readerList.push({
+                // tslint:disable-next-line
                 length: length,
+                // tslint:disable-next-line
                 isString: isString,
-                string_end_falg: end_falg,
+                // tslint:disable-next-line
+                stringEndFlag: endFalg,
+                // tslint:disable-next-line
                 callback: callback
             });
         }
     }
-    private readData<T>(length: number, converter: (buffer: Buffer) => T, isString: boolean = false, end_falg: 0 | 10 = 0): Promise<T> {
+    private readData<T>(length: number, converter: (buffer: Buffer) => T, isString: boolean = false, endFalg: 0 | 10 = 0): Promise<T> {
         return new Promise<T>((resolve, reject) => {
-            this.readBuffer(length, function (err: Error | null, buffer?: Buffer) {
+            this.readBuffer(length, (err: Error | null, buffer?: Buffer)=>{
                 if (err) {
                     reject(err);
                 }
@@ -129,15 +135,16 @@ class Socket extends netSocket {
                     resolve(converter(buffer));
                 }
                 else {
-                    let _err_ = new Error(`buffer cant be null or undefined`);
-                    reject(_err_);
+                    const nullError = new SocketError(`buffer cant be null or undefined`);
+                    nullError.code="BUFFER_IS_NULL";
+                    reject(nullError);
                 }
 
-            }, isString, end_falg);
+            }, isString, endFalg);
         });
     }
 
-    readString(encoding: BufferEncoding | number = "utf8", length: number = Number.MAX_VALUE, end_falg: 0 | 10 = 0): Promise<string> {
+    readString(encoding: BufferEncoding | number = "utf8", length: number = Number.MAX_VALUE, endFalg: 0 | 10 = 0): Promise<string> {
         let enc: BufferEncoding, len: number;
         if (typeof encoding === "string") {
             enc = encoding;
@@ -151,7 +158,7 @@ class Socket extends netSocket {
             enc = "utf8"
             len = length;
         }
-        return this.readData<string>(len, buffer => buffer.toString(enc), true, end_falg);
+        return this.readData<string>(len, buffer => buffer.toString(enc), true, endFalg);
     }
     readLine(encoding: BufferEncoding | number = "utf8", length: number = Number.MAX_VALUE): Promise<string> {
         return this.readString(encoding, length, 10);
@@ -231,128 +238,129 @@ class Socket extends netSocket {
     }
     //
     writeDoubleBE(value: number): void {
-        let buffer = Buffer.alloc(8);
+        const buffer = Buffer.alloc(8);
         buffer.writeDoubleBE(value);
         this.write(buffer);
     }
     writeDoubleLE(value: number): void {
-        let buffer = Buffer.alloc(8);
+        const buffer = Buffer.alloc(8);
         buffer.writeDoubleLE(value);
         this.write(buffer);
     }
     writeFloatBE(value: number): void {
-        let buffer = Buffer.alloc(4);
+        const buffer = Buffer.alloc(4);
         buffer.writeFloatBE(value);
         this.write(buffer);
     }
     writeFloatLE(value: number): void {
-        let buffer = Buffer.alloc(4);
+        const buffer = Buffer.alloc(4);
         buffer.writeFloatLE(value);
         this.write(buffer);
     }
     writeInt8(value: number): void {
-        let buffer = Buffer.alloc(1);
+        const buffer = Buffer.alloc(1);
         buffer.writeInt8(value);
         this.write(buffer);
     }
     writeInt16BE(value: number): void {
-        let buffer = Buffer.alloc(2);
+        const buffer = Buffer.alloc(2);
         buffer.writeInt16BE(value);
         this.write(buffer);
     }
     writeInt16LE(value: number): void {
-        let buffer = Buffer.alloc(2);
+        const buffer = Buffer.alloc(2);
         buffer.writeInt16LE(value);
         this.write(buffer);
     }
     writeInt32BE(value: number): void {
-        let buffer = Buffer.alloc(4);
+        const buffer = Buffer.alloc(4);
         buffer.writeInt32BE(value);
         this.write(buffer);
     }
     writeInt32LE(value: number): void {
-        let buffer = Buffer.alloc(4);
+        const buffer = Buffer.alloc(4);
         buffer.writeInt32LE(value);
         this.write(buffer);
     }
     writeIntBE(value: number, byteLength: number): void {
-        let buffer = Buffer.alloc(byteLength);
+        const buffer = Buffer.alloc(byteLength);
         buffer.writeIntBE(value, 0, byteLength);
         this.write(buffer);
     }
     writeIntLE(value: number, byteLength: number): void {
-        let buffer = Buffer.alloc(byteLength);
+        const buffer = Buffer.alloc(byteLength);
         buffer.writeIntLE(value, 0, byteLength);
         this.write(buffer);
     }
     writeUInt8(value: number): void {
-        let buffer = Buffer.alloc(1);
+        const buffer = Buffer.alloc(1);
         buffer.writeUInt8(value);
         this.write(buffer);
     }
     writeUInt16BE(value: number): void {
-        let buffer = Buffer.alloc(2);
+        const buffer = Buffer.alloc(2);
         buffer.writeUInt16BE(value);
         this.write(buffer);
     }
     writeUInt16LE(value: number): void {
-        let buffer = Buffer.alloc(2);
+        const buffer = Buffer.alloc(2);
         buffer.writeUInt16LE(value);
         this.write(buffer);
     }
     writeUInt32BE(value: number): void {
-        let buffer = Buffer.alloc(4);
+        const buffer = Buffer.alloc(4);
         buffer.writeUInt32BE(value);
         this.write(buffer);
     }
     writeUInt32LE(value: number): void {
-        let buffer = Buffer.alloc(4);
+        const buffer = Buffer.alloc(4);
         buffer.writeUInt32LE(value);
         this.write(buffer);
     }
     writeUIntBE(value: number, byteLength: number): void {
-        let buffer = Buffer.alloc(byteLength);
+        const buffer = Buffer.alloc(byteLength);
         buffer.writeUIntBE(value, 0, byteLength);
         this.write(buffer);
     }
     writeUIntLE(value: number, byteLength: number): void {
-        let buffer = Buffer.alloc(byteLength);
+        const buffer = Buffer.alloc(byteLength);
         buffer.writeUIntLE(value, 0, byteLength);
         this.write(buffer);
     }
     writeBigInt64BE(value: bigint): void {
-        let buffer = Buffer.alloc(8);
+        const buffer = Buffer.alloc(8);
         buffer.writeBigInt64BE(value);
         this.write(buffer);
     }
     writeBigInt64LE(value: bigint): void {
-        let buffer = Buffer.alloc(8);
+        const buffer = Buffer.alloc(8);
         buffer.writeBigInt64LE(value);
         this.write(buffer);
     }
     writeBigUInt64BE(value: bigint): void {
-        let buffer = Buffer.alloc(8);
+        const buffer = Buffer.alloc(8);
         buffer.writeBigUInt64BE(value);
         this.write(buffer);
     }
     writeBigUInt64LE(value: bigint): void {
-        let buffer = Buffer.alloc(8);
+        const buffer = Buffer.alloc(8);
         buffer.writeBigUInt64LE(value);
         this.write(buffer);
     }
     writeBigUint64BE(value: bigint): void {
-        let buffer = Buffer.alloc(8);
+        const buffer = Buffer.alloc(8);
         buffer.writeBigUint64BE(value);
         this.write(buffer);
     }
     writeBigUint64LE(value: bigint): void {
-        let buffer = Buffer.alloc(8);
+        const buffer = Buffer.alloc(8);
         buffer.writeBigUint64LE(value);
         this.write(buffer);
     }
 
 }
 function connect(options: NetConnectOpts, connectListener?: () => void): Socket;
+// tslint:disable-next-line
 function connect(path: string, connectListener?: () => void): Socket;
 function connect(port: number, host?: string, connectListener?: () => void): Socket;
 function connect(port: number | string | NetConnectOpts, host?: string | (() => void), connectListener?: () => void): Socket {
@@ -370,12 +378,12 @@ function connect(port: number | string | NetConnectOpts, host?: string | (() => 
         socket.connect(port,h,listener);
     }
     else if (typeof port === "string") {
-        let path: string = port, listener: (() => void) | undefined=host as (() => void);
+        const path: string = port, listener: (() => void) | undefined=host as (() => void);
         socket=new Socket();
         socket.connect(path,listener);
     }
     else {
-        let o=port,listener: (() => void) | undefined=host as (() => void);
+        const o=port,listener: (() => void) | undefined=host as (() => void);
         socket=new Socket();
         socket.connect(o,listener);
     }
@@ -386,7 +394,6 @@ function createServer(connectionListener?: (socket: Socket) => void): Server;
 function createServer(options?: ServerOpts | ((socket: Socket) => void), connectionListener?: (socket: Socket) => void): Server {
     if (typeof options === "function") {
         return new Server(undefined, options);
-        //return netCreateServer(undefined,options as ((socket: Socket) => void));
     }
     else {
         return new Server(options, connectionListener);
